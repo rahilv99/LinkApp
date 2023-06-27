171be68a4ef026d523578c8b944355c0a6a89aaf
Atul Verma <atul@triblio.com>
2023-06-27 10:19:21 -0400

    Initial Commit

---
+++ Link/LinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkApp.swift
+//  Link
+//
+//  Created by Atul Verma on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct LinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
